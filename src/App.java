d67919742c73d6d64e7709052eacd5a57eaba314
Alphabets, Alphabets <yapjiahong-p@proton.me>
2024-06-20 15:05:07 +0800

    feat(locks): things are working fine.

--- src/App.java
+++ src/App.java
@@ -8,11 +8,20 @@ public class App {
         Plane plane_two = new Plane(2, tower);
         Thread t_plane_two = new Thread(plane_two);
 
+        Plane plane_three = new Plane(3, tower);
+        Thread t_plane_three = new Thread(plane_three);
+
+        Plane plane_four = new Plane(4, tower);
+        Thread t_plane_four = new Thread(plane_four);
+
         t_plane_one.start();
         t_plane_two.start();
+        t_plane_three.start();        
+        t_plane_four.start();
 
         t_plane_one.join();
         t_plane_two.join();
+        t_plane_four.join();
         
         System.out.println("Finished");
     }
--- src/Tower.java
+++ src/Tower.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Tower {
     private volatile ArrayList<Gate> gates = new ArrayList<>();
     private Runway runway;
+
+    private Lock lock = new ReentrantLock();
 
     Tower() {
         this.gates.add(new Gate(1));
@@ -13,44 +17,44 @@ public class Tower {
     }
 
     public void land(int plane_id) {
-        int unoccupied_gate = this.find_unoccupied_gate();
-        boolean can_land = unoccupied_gate != -1 && !this.runway.is_occupied();
+        System.out.println(plane_id + " requesting to land.");
+        try {
+            lock.lock();
+            int unoccupied_gate = this.find_unoccupied_gate();
+            boolean occupied_runway = this.runway.is_occupied();
+            boolean can_land = unoccupied_gate != -1 && !occupied_runway;
 
-        while(!can_land) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+            if (!can_land) {
+                System.out.println("(" + plane_id + ") Cannot land. Reason.");
+                String gate_status = (unoccupied_gate == -1) ? "No. All gates occupied." : "Yes. There is at least one free gate.";
+                System.out.println("(" + plane_id + ") Is there a free gate? " + gate_status);
+
+                System.out.println("(" + plane_id + ") Is runway occupied? " + occupied_runway);
+                return;
             }
 
-            unoccupied_gate = this.find_unoccupied_gate();
-            can_land = unoccupied_gate != -1 && !this.runway.is_occupied();
-        }
-        
-        synchronized (this.runway) {
             this.runway.occupy();
-
             System.out.println("Plane " + plane_id + " coasting to gate " + unoccupied_gate);
-            synchronized (this.gates) {
-                for (var gate : gates) {
-                    if (gate.get_id() == unoccupied_gate) {
-                        gate.occupy();
-                    }
+
+            for (var gate : gates) {
+                if (gate.get_id() == unoccupied_gate) {
+                    gate.occupy();
                 }
             }
+
             this.runway.free();
+        } finally {
+            lock.unlock();
         }
     }
 
     private int find_unoccupied_gate() {
-        synchronized (this.gates) {
-            for (var gate : gates) {
-                if (!gate.is_occupied()) {
-                    System.out.println("Unoccupied gate found.");
-                    return gate.get_id();
-                }
+        for (var gate : gates) {
+            if (!gate.is_occupied()) {
+                System.out.println("Unoccupied gate found.");
+                return gate.get_id();
             }
+
         }
 
         return -1;
