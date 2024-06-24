7017b34308fe9e1743d19e979aa04ba2353e9a46
Alphabets, Alphabets <yapjiahong-p@proton.me>
2024-06-24 21:38:55 +0800

    minor changes to simulation

--- src/App.java
+++ src/App.java
@@ -3,33 +3,26 @@ public class App {
     
     public static void main(String[] args) throws Exception {
         Tower tower = new Tower();
-        // Thread refuThread = new Thread(refuelling_truck);
         
         Plane plane_one = new Plane(1, tower);
         Plane plane_two = new Plane(2, tower);
-        Plane plane_three = new Plane(3, tower);
-        // Thread t_plane_four = new Thread(plane_four);
+        Plane plane_three = new Plane(3, tower);        
+        Plane plane_four = new Plane(4, tower);
 
         Thread t_plane_one = new Thread(plane_one);
         Thread t_plane_two = new Thread(plane_two);
         Thread t_plane_three = new Thread(plane_three);
-        
-        // Plane plane_four = new Plane(4, tower);
-
+        Thread t_plane_four = new Thread(plane_four);
 
         t_plane_one.start();
         t_plane_two.start();
-        // t_plane_three.start();        
-        // t_plane_four.start();
-
-        // refuThread.start();
+        t_plane_three.start();        
+        t_plane_four.start();
 
         t_plane_one.join();
         t_plane_two.join();
-        // t_plane_three.join();     
-        // t_plane_four.join();
-
-        // refuThread.join();
+        t_plane_three.join();     
+        t_plane_four.join();
         
         System.out.println("Finished");
     }
