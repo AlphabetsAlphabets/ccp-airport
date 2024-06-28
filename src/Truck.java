e955dca43ba43d99bd6452a44fbfd6a8e8d60c8c
Alphabets, Alphabets <yapjiahong-p@proton.me>
2024-06-28 15:00:20 +0800

    fix(refuel): It wasn't working properly. There was a deadlock scenario.

--- src/Truck.java
+++ src/Truck.java
@@ -1,10 +1,13 @@
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Truck implements Runnable {
     // Information from blocking queue:
     // https://www.baeldung.com/java-blocking-queue
     private BlockingQueue<Plane> queue;
     private String threadName;
+    private Lock lock = new ReentrantLock();
 
     Truck(BlockingQueue<Plane> queue) {
         this.queue = queue;
@@ -15,20 +18,32 @@ public class Truck implements Runnable {
         threadName = Thread.currentThread().getName();
         System.out.println(threadName + "Fuel truck ready and waiting!");
 
-        while (!queue.isEmpty()) {
-            Plane plane = queue.peek(); // first check if plane has landed.
-            if (plane.readyToRefuel.get() == true) {
-                plane = queue.poll(); // if landed, then remove from the queue.
+        try {
+            lock.lock();
+
+            while (!queue.isEmpty()) {
+                Plane plane = queue.poll();
+
+                if (!plane.readyToRefuel.get()) {
+                    // If the plane isn't ready, put it back into the queue
+                    // to avoid checking it again. Then check the next plane.
+                    queue.add(plane);
+                    continue;
+                }
+
                 System.out.println(threadName + "Currently servicing Plane " + plane.id);
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
+
                 System.out.println(threadName + "Done with Plane " + plane.id);
                 plane.readyToRefuel.set(false); // To signal the plane has a full tank.
             }
+        } finally {
+            lock.unlock();
         }
 
         System.out.println(threadName + "No more planes to service.");
